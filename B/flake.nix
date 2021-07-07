0a2efd6cc7a6f5c42d09801d8105cea60ffa2a20
David Arnold <dgx.arnold@gmail.com>
2021-07-07 16:20:04 -0500

    b6e4172a37a3d912396367ae32333b5d2dfb5a0d was resolved via https://github.com/NixOS/nix/pull/4641/commits/eae41a60287e7f378699862c90011742c64778a5

--- B/flake.nix
+++ B/flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     C = {
-      url = "path:./B/C";
+      url = "path:./C";
       inputs = {
         nixpkgs.follows = "nixpkgs";
       };
